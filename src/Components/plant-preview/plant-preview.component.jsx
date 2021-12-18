5d41aef94c18144decc331e6065be2ae3db08fe9
benzkittisak <6206021621082@fitm.kmutnb.ac.th>
2021-12-18 16:28:14 +0700

    เพิ่มส่วนของราคาสินค้าในตัว plant preview และทำปุ่มเพิ่มลงตะกร้าสินค้า

--- src/Components/plant-preview/plant-preview.component.jsx
+++ src/Components/plant-preview/plant-preview.component.jsx
@@ -4,6 +4,11 @@ import "./plant-preview.style.scss";
 
 const PlantPreview = ({ plants }) => {
   const { title, routeName } = plants;
+  const currencyFormatter = new Intl.NumberFormat('th-TH' , {
+    style:'currency',
+    currency:'THB'
+  });
+
   return (
     // <div className="product" key={id}>
     //   <span className="product-name">{name}</span>
@@ -14,10 +19,16 @@ const PlantPreview = ({ plants }) => {
         {title}
       </Link>
       <div className="plant-preview">
-        {plants.items.slice(0, 4).map(({ id, name, imageUrl }) => (
+        {plants.items.slice(0, 4).map(({ id, name, imageUrl , price }) => (
           <div className="product" key={id}>
             <span className="product-name">{name}</span>
             <img src={imageUrl} className="product-image" />
+            <span className="product-price">{currencyFormatter.format(price)}</span>
+            <div className="hover">
+              <div className="add-to-cart">
+                เพิ่มลงตะกร้าสินค้า
+              </div>
+            </div>
           </div>
         ))}
       </div>
